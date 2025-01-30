81c3cdda9a5fc16ac4f16042f32c947ffae81c19
Emmett Verba <emmettverba@gmail.com>
2025-01-30 14:43:07 -0800

    updates ESlint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       // note you must disable the base rule
       // as it can report incorrect errors
+      "@typescript-eslint/no-explicit-any": "off",
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn", // or "error"
